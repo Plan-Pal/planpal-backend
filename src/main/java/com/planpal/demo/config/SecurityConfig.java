6d095a0c26f7075cbc40f4a5112a9c2d71293fe4
iiqcov <iiqcov@gmail.com>
2024-02-16 15:32:48 +0900

    [feat] #5 cors 설정 추가

--- src/main/java/com/planpal/demo/config/SecurityConfig.java
+++ src/main/java/com/planpal/demo/config/SecurityConfig.java
@@ -7,10 +7,26 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true); // json을 자바스크립트에서 처리 o
+        config.addExposedHeader("*"); // 노출 헤더 설정
+        config.addAllowedOriginPattern("*"); // 모든 ip의 응답 o
+        config.addAllowedHeader("*"); // 모든 header의 응답 0
+        config.addAllowedMethod("*"); // 모든 메서드 응답 o
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);    // 모든 경로에 Cors o
+        return source;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
