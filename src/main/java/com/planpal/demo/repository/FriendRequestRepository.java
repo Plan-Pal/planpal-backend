4ab67bcb75e0a017822f4a088bb06b9d807adfcc
iboyeon0816 <iboyeon0816@gmail.com>
2024-02-23 01:19:33 +0900

    [feat] 친구 추가 요청 삭제 API 구현

--- src/main/java/com/planpal/demo/repository/FriendRequestRepository.java
+++ src/main/java/com/planpal/demo/repository/FriendRequestRepository.java
@@ -4,6 +4,10 @@ import com.planpal.demo.domain.User;
 import com.planpal.demo.domain.mapping.FriendRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FriendRequestRepository extends JpaRepository<FriendRequest, Long> {
     boolean existsBySenderAndReceiver(User sender, User receiver);
+
+    Optional<FriendRequest> findBySenderAndReceiver(User sender, User receiver);
 }
