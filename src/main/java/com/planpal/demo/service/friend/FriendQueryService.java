6d4e2d1b0045e84b1861cfe38725dbfe964a5751
iboyeon0816 <iboyeon0816@gmail.com>
2024-02-23 21:24:19 +0900

    [feat] 친구 목록 조회 API 구현

--- src/main/java/com/planpal/demo/service/friend/FriendQueryService.java
+++ src/main/java/com/planpal/demo/service/friend/FriendQueryService.java
@@ -2,6 +2,7 @@ package com.planpal.demo.service.friend;
 
 import com.planpal.demo.apipayload.status.ErrorStatus;
 import com.planpal.demo.domain.User;
+import com.planpal.demo.domain.mapping.Friend;
 import com.planpal.demo.domain.mapping.FriendRequest;
 import com.planpal.demo.exception.ex.UserException;
 import com.planpal.demo.repository.UserRepository;
@@ -9,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -18,15 +20,39 @@ public class FriendQueryService {
 
     private final UserRepository userRepository;
 
-    public List<FriendRequest> getSentFriendRequest(Long userId) {
+    public List<FriendRequest> getSentFriendRequests(Long userId) {
         User sender = userRepository.findById(userId)
                 .orElseThrow(() -> new UserException(ErrorStatus.USER_NOT_FOUND));
         return sender.getSentRequests();
     }
 
-    public List<FriendRequest> getReceivedFriendRequest(Long userId) {
+    public List<FriendRequest> getReceivedFriendRequests(Long userId) {
         User receiver = userRepository.findById(userId)
                 .orElseThrow(() -> new UserException(ErrorStatus.USER_NOT_FOUND));
         return receiver.getReceivedRequests();
+    }
+
+    public List<User> getFriends(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserException(ErrorStatus.USER_NOT_FOUND));
+
+        return findFriends(user);
+    }
+
+    private static List<User> findFriends(User user) {
+        List<User> friends = new ArrayList<>();
+
+        List<User> friendsAsUser1 = user.getFriendsAsUser1().stream()
+                .map(Friend::getUser2)
+                .toList();
+
+        List<User> friendsAsUser2 = user.getFriendsAsUser2().stream()
+                .map(Friend::getUser1)
+                .toList();
+
+        friends.addAll(friendsAsUser1);
+        friends.addAll(friendsAsUser2);
+
+        return friends;
     }
 }
--- src/main/java/com/planpal/demo/web/controller/FriendGetController.java
+++ src/main/java/com/planpal/demo/web/controller/FriendGetController.java
@@ -2,7 +2,9 @@ package com.planpal.demo.web.controller;
 
 import com.planpal.demo.apipayload.ApiResponse;
 import com.planpal.demo.converter.FriendConverter;
+import com.planpal.demo.converter.UserConverter;
 import com.planpal.demo.service.friend.FriendQueryService;
+import com.planpal.demo.web.dto.UserResponseDto.GetResultDto;
 import com.planpal.demo.web.dto.friend.FriendResponseDto.GetReceivedFriendRequestDto;
 import com.planpal.demo.web.dto.friend.FriendResponseDto.GetSentFriendRequestDto;
 import lombok.RequiredArgsConstructor;
@@ -21,18 +23,26 @@ public class FriendGetController {
     private final FriendQueryService friendQueryService;
 
     @GetMapping("/request/sent")
-    public ApiResponse<List<GetSentFriendRequestDto>> getSentFriendRequest(@AuthenticationPrincipal Long userId) {
-        List<GetSentFriendRequestDto> getRequestDtos = friendQueryService.getSentFriendRequest(userId).stream()
+    public ApiResponse<List<GetSentFriendRequestDto>> getSentFriendRequests(@AuthenticationPrincipal Long userId) {
+        List<GetSentFriendRequestDto> getRequestDtos = friendQueryService.getSentFriendRequests(userId).stream()
                 .map(FriendConverter::toGetSentFriendRequestDto)
                 .toList();
         return ApiResponse.onSuccess(getRequestDtos);
     }
 
     @GetMapping("/request/received")
-    public ApiResponse<List<GetReceivedFriendRequestDto>> getReceivedFriendRequest(@AuthenticationPrincipal Long userId) {
-        List<GetReceivedFriendRequestDto> getRequestDtos = friendQueryService.getReceivedFriendRequest(userId).stream()
+    public ApiResponse<List<GetReceivedFriendRequestDto>> getReceivedFriendRequests(@AuthenticationPrincipal Long userId) {
+        List<GetReceivedFriendRequestDto> getRequestDtos = friendQueryService.getReceivedFriendRequests(userId).stream()
                 .map(FriendConverter::toGetReceivedFriendRequestDto)
                 .toList();
         return ApiResponse.onSuccess(getRequestDtos);
+    }
+
+    @GetMapping
+    public ApiResponse<List<GetResultDto>> getFriends(@AuthenticationPrincipal Long userId) {
+        List<GetResultDto> getResultDtos = friendQueryService.getFriends(userId).stream()
+                .map(UserConverter::toGetResultDto)
+                .toList();
+        return ApiResponse.onSuccess(getResultDtos);
     }
 }
