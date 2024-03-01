0cb30e6d35b9ff7265fdf1d242f9ca64a7426527
iiqcov <iiqcov@gmail.com>
2024-03-01 22:36:48 +0900

    [feat] 전체 일정 조회시 짧은 제목이 포함되도록 수정

--- src/main/java/com/planpal/demo/web/dto/schedule/GetSimpleScheduleResponse.java
+++ src/main/java/com/planpal/demo/web/dto/schedule/GetSimpleScheduleResponse.java
@@ -7,14 +7,17 @@ import java.time.LocalDateTime;
 
 @Getter
 public class GetSimpleScheduleResponse {
+    private final String shortTitle;
     private final String title;
     private final LocalDateTime appointedTime;
     private final String place;
 
     @Builder
-    public GetSimpleScheduleResponse(String title,
+    public GetSimpleScheduleResponse(String shortTitle,
+                                     String title,
                                      LocalDateTime appointedTime,
                                      String place){
+        this.shortTitle=shortTitle;
         this.title=title;
         this.appointedTime=appointedTime;
         this.place=place;
