040be78cbbacccda8b8841b2c586270ec25e5ff7
iiqcov <iiqcov@gmail.com>
2024-03-21 22:55:36 +0900

    [feat] 요약된 일정 조회시 id가 포함되도록 수정

--- src/main/java/com/planpal/demo/web/dto/schedule/GetSimpleScheduleResponse.java
+++ src/main/java/com/planpal/demo/web/dto/schedule/GetSimpleScheduleResponse.java
@@ -7,19 +7,22 @@ import java.time.LocalDateTime;
 
 @Getter
 public class GetSimpleScheduleResponse {
+    private final Long scheduleId;
     private final String shortTitle;
     private final String title;
     private final LocalDateTime appointedTime;
     private final String place;
 
     @Builder
-    public GetSimpleScheduleResponse(String shortTitle,
+    public GetSimpleScheduleResponse(Long scheduleId,
+                                     String shortTitle,
                                      String title,
                                      LocalDateTime appointedTime,
                                      String place){
-        this.shortTitle=shortTitle;
-        this.title=title;
-        this.appointedTime=appointedTime;
-        this.place=place;
+        this.scheduleId = scheduleId;
+        this.shortTitle = shortTitle;
+        this.title = title;
+        this.appointedTime = appointedTime;
+        this.place = place;
     }
 }
